cc623dcb3c4b75c131afe41befcafaa2cf8a488b
ArtemisSoftware <gustavo.isaac@gmail.com>
2019-12-01 16:41:14 +0000

    Extracting a View Model

--- MVVMTutorial/Views/PlaylistsPage.xaml.cs
+++ MVVMTutorial/Views/PlaylistsPage.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using MVVMTutorial.Models;
+using MVVMTutorial.ViewModels;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
@@ -14,40 +15,28 @@ namespace MVVMTutorial.Views
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class PlaylistsPage : ContentPage
     {
-        private ObservableCollection<Playlist> _playlists = new ObservableCollection<Playlist>();
-
+        
         public PlaylistsPage()
         {
+            BindingContext = new PlaylistsViewModel();
+
             InitializeComponent();
         }
 
         protected override void OnAppearing()
         {
-            playlistsListView.ItemsSource = _playlists;
-
             base.OnAppearing();
         }
 
         void OnAddPlaylist(object sender, System.EventArgs e)
         {
-            var newPlaylist = "Playlist " + (_playlists.Count + 1);
-
-            _playlists.Add(new Playlist { Title = newPlaylist });
-
-            this.Title = $"{_playlists.Count} Playlists";
+            (BindingContext as PlaylistsViewModel).AddPlaylist();
+            
         }
 
         void OnPlaylistSelected(object sender, Xamarin.Forms.SelectedItemChangedEventArgs e)
         {
-            if (e.SelectedItem == null)
-                return;
-
-            var playlist = e.SelectedItem as Playlist;
-            playlist.IsFavorite = !playlist.IsFavorite;
-
-            playlistsListView.SelectedItem = null;
-
-            //await Navigation.PushAsync (new PlaylistDetailPage(playlist));
+            (BindingContext as PlaylistsViewModel).SelectPlaylist(e.SelectedItem as Playlist);
         }
     }
 }
